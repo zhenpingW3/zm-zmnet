942d40fbac22b08435b5db78b148ddbaa2ccaacc
Benson Wang <benson.wang@moodys.com>
2024-04-25 09:30:08 -0400

    Add project files.

---
+++ ZMNet.Common/NumberExtension.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMNet.Common
+{
+  public static class NumberExtension
+  {
+    public static string Format(this double number, int decimalPlace, bool thousandthPlace = true)
+    {
+      string format = decimalPlace.GetNumberFormat(thousandthPlace);
+
+      return string.Format(format, number);
+    }
+
+    public static string GetNumberFormat(this int decimalPlace, bool thousandthPlace = true)
+    {
+      return "{0:" + (thousandthPlace ? "N" : "F") + decimalPlace + "}";
+    }
+  }
+}
---
+++ ZMNet.Common/TypeExtension.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZMNet.Common
+{
+  public static class TypeExtension
+  {
+    public static bool IsaString(this Type type)
+    {
+      return type.FullName == "System.String";
+    }
+  }
+}
